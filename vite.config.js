e7df16447236dd1ca57e94e58cde37a6874ff6ac
tatendatobaiwa <tatenda.tobaiwa@gmail.com>
2025-07-21 18:09:29 +0200

    lgtm build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ableheartsupdate/', // Set base path for GitHub Pages deployment
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'), // Alias '@' for the 'src' folder
